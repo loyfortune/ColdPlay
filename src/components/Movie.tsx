6e8da840680e360508e1e180396e7c6e2d19f224
loyfortune <loyfortuneofforkansi@gmail.com>
2026-06-09 04:00:32 +0100

    modify moviecard styling

--- src/components/Movie.tsx
+++ src/components/Movie.tsx
@@ -42,7 +42,7 @@ const Movie = ({ item }: MovieProps) => {
               <i onClick={saveMovie} className='absolute top-4 left-4 text-gray-300'>{like ? <FaHeart /> : <FaRegHeart />}</i>             
                <p className='whitespace-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center'>{item?.title}</p>
              </div>
-          <Link key={item.id} to={`/movie/${item.id}`}>
+          <Link className='w-full h-full' key={item.id} to={`/movie/${item.id}`}>
              <img className="w-full h-auto block" src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`} alt={item?.title} />
            </Link>             
            </div>
